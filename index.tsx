3d5ccf2a2396bfec47037a600a26f2837ab4f578
wavefynova-coder <wavefynova@gmail.com>
2025-11-24 18:53:34 -0300

    feat: Convert to Pure HTML/JS/CSS
    The application has been refactored from a React application to a pure HTML, JavaScript, and CSS structure. This change removes the React dependency and simplifies the project setup by loading all resources directly via CDN in `index.html`. The build process is also removed as it's no longer necessary.

--- index.tsx
+++ index.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+// This file is disabled as the application has been converted to Pure HTML/JS/CSS in index.html
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+// const rootElement = document.getElementById('root');
+// if (!rootElement) {
+//   throw new Error("Could not find root element to mount to");
+// }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(rootElement);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
